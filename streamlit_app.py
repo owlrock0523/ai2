2f59ccca38f7e486469e3f24b963a351015ac2ca
owlrock0523 <h098s02416@chunchon.gwe.hs.kr>
2025-11-26 09:24:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     labels[0] : {"texts" : ["중국식 냉면은 맛잇어"], "images" : ["https://www.esquirekorea.co.kr/resources_old/online/org_online_image/eq/71c93efd-352d-4fb4-8a98-dd1b51475442.jpg]},
     labels[1] : {"texts" : ["짜장면은 맛있어"], "images" : [https://recipe1.ezmember.co.kr/cache/recipe/2021/10/26/8f82be9c22ec2f4f9ab25363cc611b141.jpg]},
     labels[2] : {"texts" : ["짬뽕은 맛있어"], "images" : [https://img-cf.kurly.com/hdims/resize/%3E720x/quality/90/src/shop/data/goodsview/20230803/gv20000714335_1.jpg]},
-    labels[3] : {"texts" : ["탕수육은 맛있어"], "images" : [https://blog.kakaocdn.net/dna/qnIz2/btqSdtiK7C4/AAAAAAAAAAAAAAAAAAAAAK_eDgChgNlbA3DwDL3Rhp3zyE68j5YgxkTq4Zz6RRN5/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1764514799&allow_ip=&allow_referer=&signature=IgQS1EsJodJN9xYMyJB6awE7Ae8%3D]},
+    labels[3] : {"texts" : ["탕수육은 맛있어"], "images" : [https://recipe1.ezmember.co.kr/cache/recipe/2020/07/05/2e0e7c019f283bcc36d34cdee876d15b1.jpg]},
 
 }
 
